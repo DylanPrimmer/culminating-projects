fb76f2c70a9cc2f7ae941a31734ca53a24fc11fb
DylanPrimmer <36270328+DylanPrimmer@users.noreply.github.com>
2018-06-19 11:05:52 -0400

    Add files via upload

---
+++ 3 and 5 culminating/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿//Dylan Primmer
+// multiples of 3 and 5 up to 1000
+//euler
+//June 14 2018
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3_and_5_culminating
+{ 
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int i;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            int sum = 0;
+
+
+            for (int i = 3; i < 1000; i = i + 1)
+            {
+                if (i % 3 == 0)
+                {
+                    sum += i;
+                }
+                else if (i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            // show sum of multiples of 3 and 5
+            Console.WriteLine(sum);
+            MessageBox.Show(sum.ToString());
+
+
+
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿//Dylan Primmer
+//Euler Power digit sum Project
+//June 19, 2018
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace power_digit_sum_culm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+          
+            InitializeComponent();
+            int[] digits = new int[350];
+
+            
+            digits[0] = 1;
+            //2 to the power of 1000
+            for (int i = 0; i < 1000; i++)
+            {
+                //multiply digits by 2
+                for (int T = digits.Length - 2; T >= 0; T--)
+                {
+                    digits[T] *= 2;
+                    digits[T + 1] += digits[T] / 10;
+                    digits[T] %= 10;
+                }
+            }
+
+            //find sum
+            double sum = 0;
+            for (int i = 0; i < digits.Length; i++)
+            {
+                sum += digits[i];
+            }
+            //show answer
+            Console.WriteLine(sum);
+            MessageBox.Show("The sum of the product of 2^1000");
+            MessageBox.Show(sum.ToString());
+        }
+    }
+}
+    
+
